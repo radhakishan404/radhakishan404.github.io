d01e93ee946e229b59d8e81b5354f036521ba97e
radhakishan404 <radhakishanjangid404@gmail.com>
2021-01-09 23:10:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Portfolio from './components/pages/Portfolio';
+import PortfolioDetail from './components/pages/PortfolioDetail';
 
 function App() {
     return (
         <Router>
             <Route exact={true} path="/" component={Home} />
             <Route exact={true} path="/about" component={About} />
+            <Route exact={true} path="/contact" component={Contact} />
+            <Route exact={true} path="/portfolio" component={Portfolio} />
+            <Route exact={true} path="/portfolio/:topic" component={PortfolioDetail} />
         </Router>
     );
 }
