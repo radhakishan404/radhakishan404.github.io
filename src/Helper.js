06461154fb4d8f73c90b0e8cbda2c283a0f6a608
radhakishan404 <radhakishanjangid404@gmail.com>
2021-01-13 23:37:44 +0530

    Update Helper.js

--- src/Helper.js
+++ src/Helper.js
@@ -10,3 +10,34 @@ export function permalinkGenerate(val) {
 
     return permalink;
 }
+
+function formatDate() {
+    var d = new Date(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2)
+        month = '0' + month;
+    if (day.length < 2)
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
+export async function getPackageTotalDownload(name) {
+    const url = "https://api.npmjs.org/downloads/range/2020-08-01:"+formatDate()+"/"+name;
+    const response = await fetch(url, {
+        method: 'GET'
+    })
+    const data = await response.json();
+
+    let total = 0;
+
+    if(data && data.downloads) {
+        for (let i = 0; i < data.downloads.length; i++) {
+            total = total + data.downloads[i].downloads;
+        }
+    }
+    return total;
+}
