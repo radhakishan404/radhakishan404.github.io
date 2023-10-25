5215c5a414b20a6a6e9b1f1555ad6f0fd616435a
Radhakishan Jangid <radhakishanjangid404@gmail.com>
2023-10-25 12:30:37 +0530

    images and experience

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,6 +4,7 @@ import Loader from "react-animation-loader";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import softdata from "../../softdata.json";
 import ReactGA from 'react-ga';
+import { calcDate } from '../../Helper';
 ReactGA.initialize('UA-187892763-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
@@ -62,10 +63,11 @@ function About(props) {
                                         <div className="col-sm-12 col-md-9">
                                             <h1>Who's this guy?</h1>
                                             <p className="section-subtitle">
-                                                Hello Friends! I am Radhakishan Jangid. {myAge()} years old <strong className="text-gradient">Javascript and Backend developer</strong> based in Mumbai, India. My passion is web development. Over the past 2.5 year, I have gained a lot of experience with designing and developing numerious websites in React, Core PHP, Laravel, CodeIgniter, Node Js.
+                                                Hello Friends! I am Radhakishan Jangid. {myAge()} years old <strong className="text-gradient">Javascript and Backend developer</strong> based in Mumbai, India. My passion is web development. Over the past {calcDate()} year, I have gained a lot of experience with designing and developing numerious websites in <strong className="text-gradient">React.js, Next.js, Node Js, Core PHP, Laravel, CodeIgniter</strong>.
                                                 <br />
                                                 <br />
-                                                Currently, I work as Backend Developer at <strong className="text-gradient"><a href="http://www.techstalwarts.com/" target="_blank" rel="noopener noreferrer">TechStalwarts Software Development LLP</a></strong>.
+                                                I've worked with a variety of databases, including <strong className="text-gradient">MongoDB, MySQL, and PostgreSQL</strong>.
+                                                I also have extensive experience in team leadership and project management.I have successfully deployed many web applications from scratch on AWS as well as on VPS servers.
                                                 <br />
                                                 <br />
                                                 When not writing code, I really enjoy learning new stuff, playing games, doing workout in gym and specially chilling with friends. :).
@@ -124,121 +126,109 @@ function About(props) {
                                 <span className="skill-items">
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/react.svg" data-large="img/react.svg" className="blur-out" alt="React" />
+                                            <img src="https://radhakishan404.github.io/images/react.svg" data-large="img/react.svg" className="blur-out" alt="React" />
                                         </span>
-                                        <span>React</span>
+                                        <span>React Js</span>
                                     </span>
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/nodejs.svg" data-large="img/nodejs.svg" className="blur-out" alt="Node JS" />
+                                            <img src="https://radhakishan404.github.io/images/nodejs.svg" data-large="img/nodejs.svg" className="blur-out" alt="Node JS" />
                                         </span>
                                         <span>Node.js</span>
                                     </span>
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/typescript.svg" data-large="img/typescript.svg" className="blur-out" alt="TypeScript" />
+                                            <img src="https://radhakishan404.github.io/images/typescript.svg" data-large="img/typescript.svg" className="blur-out" alt="TypeScript" />
                                         </span>
                                         <span>TypeScript</span>
                                     </span>
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/npm.svg" data-large="img/npm.svg" className="blur-out" alt="NPM" />
+                                            <img src="https://radhakishan404.github.io/images/npm.svg" data-large="img/npm.svg" className="blur-out" alt="NPM" />
                                         </span>
                                         <span>NPM</span>
                                     </span>
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/js.svg" data-large="img/js.svg" alt="JavaScript" className="blur-out" />
+                                            <img src="https://radhakishan404.github.io/images/js.svg" data-large="img/js.svg" alt="JavaScript" className="blur-out" />
                                         </span>
                                         <span>JavaScript</span>
                                     </span>
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/php.svg" data-large="img/php.svg" alt="PHP" className="blur-out" />
+                                            <img src="https://radhakishan404.github.io/images/php.svg" data-large="img/php.svg" alt="PHP" className="blur-out" />
                                         </span>
                                         <span>PHP</span>
                                     </span>
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/codeigniter.svg" data-large="img/codeigniter.svg" alt="CodeIgniter" className="blur-out" />
+                                            <img src="https://radhakishan404.github.io/images/codeigniter.svg" data-large="img/codeigniter.svg" alt="CodeIgniter" className="blur-out" />
                                         </span>
                                         <span>CodeIgniter</span>
                                     </span>
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/jquery.svg" data-large="img/jquery.svg" alt="jQuery" className="blur-out" />
+                                            <img src="https://radhakishan404.github.io/images/jquery.svg" data-large="img/jquery.svg" alt="jQuery" className="blur-out" />
                                         </span>
                                         <span>jQuery</span>
                                     </span>
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/html.svg" data-large="img/html.svg" alt="HTML" className="blur-out" />
+                                            <img src="https://radhakishan404.github.io/images/html.svg" data-large="img/html.svg" alt="HTML" className="blur-out" />
                                         </span>
                                         <span>HTML</span>
                                     </span>
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/css.svg" data-large="img/css.svg" alt="CSS3" className="blur-out" />
+                                            <img src="https://radhakishan404.github.io/images/css.svg" data-large="img/css.svg" alt="CSS3" className="blur-out" />
                                         </span>
                                         <span>CSS3</span>
                                     </span>
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/bootstrap.svg" data-large="img/bootstrap.svg" alt="Bootstrap" className="blur-out" />
+                                            <img src="https://radhakishan404.github.io/images/bootstrap.svg" data-large="img/bootstrap.svg" alt="Bootstrap" className="blur-out" />
                                         </span>
                                         <span>Bootstrap</span>
                                     </span>
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/mysql.svg" data-large="img/mysql.svg" alt="MySQL" className="blur-out" />
+                                            <img src="https://radhakishan404.github.io/images/mysql.svg" data-large="img/mysql.svg" alt="MySQL" className="blur-out" />
                                         </span>
                                         <span>MySQL</span>
                                     </span>
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/sql.png" data-large="img/sql.png" className="blur-out" alt="SQL" />
+                                            <img src="https://radhakishan404.github.io/images/sql.svg" data-large="img/sql.svg" className="blur-out" alt="SQL" />
                                         </span>
                                         <span>SQL</span>
                                     </span>
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/phpadmin.svg" data-large="img/phpadmin.svg" className="blur-out" alt="PhpMyAdmin" />
-                                        </span>
-                                        <span>PhpMyAdmin</span>
-                                    </span>
-                                    <span className="skill-item">
-                                        <span>
-                                            <img src="https://radhakishan.site/img/adminer.png" data-large="img/adminer.png" className="blur-out" alt="Adminer" />
+                                            <img src="https://radhakishan404.github.io/images/adminer.png" data-large="img/adminer.png" className="blur-out" alt="Adminer" />
                                         </span>
                                         <span>Adminer</span>
                                     </span>
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/git.svg" data-large="img/git.svg" className="blur-out" alt="MySQL" />
+                                            <img src="https://radhakishan404.github.io/images/git.svg" data-large="img/git.svg" className="blur-out" alt="MySQL" />
                                         </span>
                                         <span>Git</span>
                                     </span>
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/aws.png" data-large="img/aws.png" className="blur-out" alt="Amazon S3" />
+                                            <img src="https://radhakishan404.github.io/images/aws.svg" data-large="img/aws.svg" className="blur-out" alt="Amazon S3" />
                                         </span>
                                         <span>Amazon S3</span>
                                     </span>
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/aws.png" data-large="img/aws.png" className="blur-out" alt="Amazon Web Services (AWS)" />
+                                            <img src="https://radhakishan404.github.io/images/aws.svg" data-large="img/aws.svg" className="blur-out" alt="Amazon Web Services (AWS)" />
                                         </span>
                                         <span>Amazon Web Services (AWS)</span>
                                     </span>
                                     <span className="skill-item">
                                         <span>
-                                            <img src="https://radhakishan.site/img/ionic.svg" data-large="img/ionic.svg" className="blur-out" alt="Ionic" />
-                                        </span>
-                                        <span>Ionic</span>
-                                    </span>
-                                    <span className="skill-item">
-                                        <span>
-                                            <img src="https://radhakishan.site/img/photoshop.svg" data-large="img/photoshop.svg" className="blur-out" alt="Photoshop" />
+                                            <img src="https://radhakishan404.github.io/images/photoshop.svg" data-large="img/photoshop.svg" className="blur-out" alt="Photoshop" />
                                         </span>
                                         <span>Photoshop</span>
                                     </span>
